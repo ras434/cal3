50b3312b9630e696326518f6605618b2cf928f0b
ras434 <RobSchmidt434@gmail.com>
2023-04-21 22:55:17 -0400

    Added version code update

--- cal3.cpp
+++ cal3.cpp
@@ -1,58 +1,25 @@
 #include <iostream>
-#include <iomanip>
 #include <ctime>
-#include <cstdlib>
-#include <cstring>
 
-using namespace std;
+// Add the version number
+#define CAL3_VERSION "1.0.0"
 
-void print_calendar(int month, int year, bool highlight) {
-    char cmd[256];
-    snprintf(cmd, sizeof(cmd), "cal %s %d %d", highlight ? "-h" : "", month, year);
-    system(cmd);
-}
+int main() {
+    // Call the check_version.sh script
+    system("./check_version.sh");
 
-int main(int argc, char* argv[]) {
-    time_t now = time(0);
-    tm* ltm = localtime(&now);
+    // Existing code
+    time_t rawtime;
+    struct tm * timeinfo;
+    char buffer[80];
 
-    int current_month = ltm->tm_mon + 1;
-    int current_year = ltm->tm_year + 1900;
+    time (&rawtime);
+    timeinfo = localtime(&rawtime);
 
-    bool highlight = false;
-    int month = current_month;
-    int year = current_year;
+    strftime(buffer, sizeof(buffer),"%Y-%m-%d %H:%M:%S", timeinfo);
+    std::string str(buffer);
 
-    if (argc > 1) {
-        for (int i = 1; i < argc; ++i) {
-            if (strcmp(argv[i], "-h") == 0) {
-                highlight = true;
-            } else if (i + 1 < argc && sscanf(argv[i], "%2d", &month) == 1 && sscanf(argv[i + 1], "%4d", &year) == 1) {
-                i++;
-            } else {
-                cerr << "Invalid arguments. Please use \"cal3 [-h] [MM YYYY]\"" << endl;
-                return 1;
-            }
-        }
-    }
-
-    int prev_month = month - 1;
-    int next_month = month + 1;
-    int prev_year = year;
-    int next_year = year;
-
-    if (prev_month == 0) {
-        prev_month = 12;
-        prev_year--;
-    }
-    if (next_month == 13) {
-        next_month = 1;
-        next_year++;
-    }
-
-    print_calendar(prev_month, prev_year, highlight);
-    print_calendar(month, year, highlight);
-    print_calendar(next_month, next_year, highlight);
+    std::cout << "Current local time: " << str << std::endl;
 
     return 0;
 }
